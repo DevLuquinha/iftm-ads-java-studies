5344a09f4e35d19ca0156e1cd3691ab1a2947eaf
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2026-02-19 18:27:26 -0300

    FEAT: Print hello world in Java

---
+++ poo/01-classroom-exercises/01-learn-java-syntax/src/classroom/Exercise01.java
@@ -0,0 +1,10 @@
+package classroom;
+
+/*
+01 - Imprima todos os números de 150 a 300.
+*/
+public class Exercise01{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
