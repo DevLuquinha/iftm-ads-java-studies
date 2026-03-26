2085b562b1cf291222b57fb905c1d0ed64a56239
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2026-03-25 21:24:39 -0300

    Create a client user with overload constructors

---
+++ oop/01-classroom-exercises/src/classroom/class_26_03_25/Client.java
@@ -0,0 +1,67 @@
+package classroom.class_26_03_25;
+
+public class Client {
+    private String name = "";
+    private int age = 0;
+    private String cpf = "";
+    private double height = 0.0;
+
+    public Client(String name){
+        this.name = name;
+    }
+
+    public Client(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public Client(String name, int age, String cpf){
+        this.name = name;
+        this.age = age;
+        this.cpf = cpf;
+    }
+
+    public Client(String name, int age, String cpf, double height){
+        this.name = name;
+        this.age = age;
+        this.cpf = cpf;
+        this.height = height;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return "The " + name + " with the CPF equals " + cpf + " has " + height + "m and is " + age + "years old";
+    }
+}
---
+++ oop/01-classroom-exercises/src/classroom/class_26_03_25/Main.java
@@ -0,0 +1,7 @@
+package classroom.class_26_03_25;
+
+public class Main {
+    static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
